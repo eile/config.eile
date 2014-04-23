b9bfafbefa3b3f721c00a13e68618ce078564030
Stefan Eilemann <Stefan.Eilemann@epfl.ch>
2014-04-23 12:12:01 +0200

    Add point cloud renderer

---
+++ CS446.cmake
@@ -0,0 +1,6 @@
+
+set(CS446_PACKAGE_VERSION 3.1)
+set(CS446_REPO_URL https://github.com/eile/CS446.git)
+set(CS446_DEPENDS REQUIRED OpenMesh GLUT OpenGL)
+set(CS446_DEB_DEPENDS )
+set(CS446_PORT_DEPENDS )
