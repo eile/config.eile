c9109694a885981deb2b97888353f7cea06cceee
Stefan Eilemann <Stefan.Eilemann@epfl.ch>
2014-04-25 10:10:42 +0200

    Add point cloud renderer

---
+++ pcl.cmake
@@ -0,0 +1,8 @@
+
+set(PCL_PACKAGE_VERSION 2.2)
+set(PCL_REPO_URL https://github.com/PointCloudLibrary/pcl.git)
+set(PCL_USER_URL https://github.com/eile/pcl.git)
+set(PCL_DEPENDS )
+set(PCL_DEB_DEPENDS )
+set(PCL_PORT_DEPENDS openni vtk5 eigen3-devel flann qhull)
+set(PCL_FORCE_BUILD ${CI_BUILD})
