f242c7b54c8efb8e1ec7b3382b5cd6e435f5ebd5
Stefan Eilemann <Stefan.Eilemann@epfl.ch>
2014-05-03 12:00:04 +0200

    Tweak ceph config

--- pcl.cmake
+++ pcl.cmake
@@ -2,7 +2,7 @@
 set(PCL_PACKAGE_VERSION 2.2)
 set(PCL_REPO_URL https://github.com/PointCloudLibrary/pcl.git)
 set(PCL_USER_URL https://github.com/eile/pcl.git)
-set(PCL_DEPENDS )
+set(PCL_DEPENDS REQUIRED OpenGL)
 set(PCL_DEB_DEPENDS libopenni-dev  libvtk5-dev libeigen3-dev libflann-dev
   libqhull-dev)
 set(PCL_PORT_DEPENDS openni vtk5 eigen3-devel flann qhull)
