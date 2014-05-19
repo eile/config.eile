5f88661f0cb24cc9b4feab423bde01ff962afa4d
Stefan Eilemann <Stefan.Eilemann@epfl.ch>
2014-05-19 12:03:15 +0200

    ITK examples config, minor pcl tweaks

--- pcl.cmake
+++ pcl.cmake
@@ -5,5 +5,6 @@ set(PCL_USER_URL https://github.com/eile/pcl.git)
 set(PCL_DEPENDS REQUIRED OpenGL)
 set(PCL_DEB_DEPENDS libopenni-dev  libvtk5-dev libeigen3-dev libflann-dev
   libqhull-dev)
-set(PCL_PORT_DEPENDS openni vtk5 eigen3-devel flann qhull)
+set(PCL_PORT_DEPENDS openni vtk5 eigen3-devel flann qhull hffzip sphinx)
+set(PCL_CMAKE_ARGS -DBUILD_simulation=ON)
 set(PCL_FORCE_BUILD ${CI_BUILD})
